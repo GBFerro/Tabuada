80b1643b4fc5ee03ee2f47a411f3bd48e7108e8e
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:40:38 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int nTabuada;
+
+        Console.WriteLine("Digite um número: ");
+        if (!int.TryParse(Console.ReadLine(), out nTabuada))
+        {
+            Console.WriteLine("Errou");
+        }
+        else
+        {
+            if ((nTabuada < 1)|(nTabuada > 9))
+            {
+                Console.WriteLine("Número deve ser entre 1 e 9");
+            }
+            else
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine($"{nTabuada} x {i} = {nTabuada*i}");
+                }
+            }
+        }
+
+    }
+}
